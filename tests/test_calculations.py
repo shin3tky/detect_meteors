6e015fbcf0c15c1be2d3eeeb0c9a811de9cae7e6
Shinichi Morita <shin3tky@gmail.com>
2025-11-26 01:31:00 +0900

    Add a comment to test_calculations.py.

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import os
 
+# Add project root directory to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from detect_meteors_cli import (
