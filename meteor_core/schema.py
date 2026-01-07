53b7bb7ba142574967f662eb58fb74438d975842
Shinichi Morita <shin3tky@gmail.com>
2026-01-07 18:52:27 +0900

    Updated version string to 1.6.8.

--- meteor_core/schema.py
+++ meteor_core/schema.py
@@ -22,7 +22,7 @@ ImageLike = Union["np.ndarray", "torch.Tensor", "Image.Image"]
 # ==========================================
 # Version
 # ==========================================
-VERSION = "1.6.6"
+VERSION = "1.6.8"
 DETECTION_CONTEXT_SCHEMA_VERSION = 1
 DETECTION_RESULT_SCHEMA_VERSION = 1
 INPUT_CONTEXT_SCHEMA_VERSION = 1
